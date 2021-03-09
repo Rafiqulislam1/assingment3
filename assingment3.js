0de85d7f08ca91d309a97ff6355d3d5d1064b07c
Rofiqulislam <rofiqulislam.antor@gmail.com>
2021-03-09 20:09:17 +0600

    first commit

---
+++ assingment3.js
@@ -0,0 +1,35 @@
+ =========kilometerToMeter===========
+ function kilometerToMeter(Kilometer) {
+     var Meter = Kilometer/1000;
+     return Meter;
+ }
+ var Meter = kilometerToMeter(2000);
+ console.log(Meter);
+
+===================
+ Mega friend
+function Find = [
+     {name: Antor},
+    {name: Rafiqul},
+    {name: Rija}
+   ];
+  
+   function Name(Max) {
+     return max.name 
+  }
+  
+   console.log(Find.find(Name));
+  
+============================
+ 
+ var  [ ReadableStreamDefaultController,antor]
+ console.localStorage()
+
+
+ =========kilometerToMeter===========
+ function kilometerToMeter(Kilometer) {
+   var Meter = Kilometer/1000;
+     return Meter;/
+     }
+var Meter = kilometerToMeter(2000);
+ console.log(Meter);
